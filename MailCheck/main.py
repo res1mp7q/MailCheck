a169120efd98e8f36dabee70400ec93a1aa3363b
Jimmy Lamont <res1mp7q@users.noreply.github.com>
2023-12-18 16:12:30 -0800

    Lookup records

--- MailCheck/main.py
+++ MailCheck/main.py
@@ -1,21 +1,32 @@
 import smtplib
 import re
-import py3dns
+import dns.resolver
 
 def get_mx_records(domain):
     try:
-        mx_records = py3dns.resolver.query(domain, 'MX')
+        resolver = dns.resolver.Resolver()
+        mx_records = resolver.query(domain, 'MX')
         return [str(mx.exchange)[:-1] for mx in mx_records]
-    except (py3dns.resolver.NoAnswer, py3dns.resolver.NXDOMAIN, py3dns.resolver.NoNameservers) as e:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers) as e:
         print(f"Error resolving MX records for {domain}: {e}")
         return []
 
 def get_a_records(domain):
     try:
-        a_records = py3dns.resolver.query(domain, 'A')
+        resolver = dns.resolver.Resolver()
+        a_records = resolver.query(domain, 'A')
         return [str(a) for a in a_records]
-    except (py3dns.resolver.NoAnswer, py3dns.resolver.NXDOMAIN, py3dns.resolver.NoNameservers) as e:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers) as e:
         print(f"Error resolving A records for {domain}: {e}")
+        return []
+
+def get_txt_records(domain):
+    try:
+        resolver = dns.resolver.Resolver()
+        txt_records = resolver.query(domain, 'TXT')
+        return [str(txt) for txt in txt_records]
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers) as e:
+        print(f"Error resolving TXT records for {domain}: {e}")
         return []
 
 def print_domain_info(email):
@@ -47,6 +58,15 @@ def print_domain_info(email):
             print(f"- {a}")
     else:
         print(f"No A records found for {domain}")
+
+    # Get and print TXT records for the domain
+    txt_records = get_txt_records(domain)
+    if txt_records:
+        print("TXT Records:")
+        for txt in txt_records:
+            print(f"- {txt}")
+    else:
+        print(f"No TXT records found for {domain}")
 
 if __name__ == "__main__":
     # Get the email address from the user
